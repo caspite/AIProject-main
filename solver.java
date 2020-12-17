d82fd7a6ab01e403cfe95ec9e9a4e624258e011c
tehila caspi <61005126+caspite@users.noreply.github.com>
2020-12-17 20:31:40 +0200

    solver bb programing

--- solver.java
+++ solver.java
@@ -1,11 +1,23 @@
 public class solver {
+    int [][] goal;
 
-    public static void Astar(Tile tile){
-        double LB=Double.POSITIVE_INFINITY;
-        double UB=0;
+    public static void Astar(Tile tile,int[][] goal){
+    goal=goal;
 
     }
-    public static void branchNbaund(Tile tile){
+    public static void branchNbaund(Tile tile,int[][] goal){
+        double UB=Double.POSITIVE_INFINITY;
+        double LB=0;
+        double currentCost=0;
+        goal=goal;
+
+        //while current position != goal
+        //get tile open list
+        //open the next position in tree- one bellow the current position
+        //check if current cost + the new position's cost is more UB
+        //if true - go to the next position -one bellow the current position
+            //if their is no next position - chang the curr position to the first position in the open list
+        //if false - current position = next // update bounds // current cost and expend the position
 
     }
 }
